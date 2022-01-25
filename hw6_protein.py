77f7740ab2f6c400c3a6b96f230f4159b237bcb9
PaVaN-02 <pavanpillalamarri222@msitprogram.net>
2022-01-25 11:33:28 +0530

    Create a Bar Chart

--- hw6_protein.py
+++ hw6_protein.py
@@ -274,7 +274,15 @@ Returns: None
 '''
 def createChart(xLabels, freqList1, label1, freqList2, label2, edgeList=None):
     import matplotlib.pyplot as plt
-    return
+    w = 0.35  # the width of the bars
+    plt.bar(xLabels, freqList1, width=-w, align='edge', label=label1)
+    plt.bar(xLabels, freqList2, width=w, align='edge', label=label2) 
+    title="Side by Side Bar Plot"
+    plt.xticks(rotation="horizontal")
+    plt.legend()
+    plt.title(title)
+    plt.show()
+    return None
 
 
 '''
